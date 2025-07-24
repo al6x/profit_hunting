63fbb25cc71b088128eb11e6b7cf44f4720102c4
Alex <alex@mail.com>
2025-07-24 19:13:05 +1000

    upd

--- lib/helpers.jl
+++ lib/helpers.jl
@@ -26,37 +26,3 @@ function cached(get::Function, id::AbstractString)
 end
 
 function to_dict(ds::DataFrame) Dict(string(c) => ds[!, c] for c in names(ds)) end
-
-# Report -------------------------------------------------------------------------------------------
-# mutable struct ReportConfig report_path; asset_path; asset_url_path; first_call end
-
-# if !isdefined(Main, :report_config)
-#   const report_config = Ref{Union{ReportConfig, Nothing}}(nothing)
-# end
-
-# configure_report!(; report_path, asset_path, asset_url_path::Union{AbstractString, Nothing}=nothing) = begin
-#   report_config[] = ReportConfig(report_path, asset_path, asset_url_path, true)
-# end
-
-# report(msg::AbstractString; print_=true) = begin
-#   turn_space_indent2_into4(text) = replace(text, r"\n  " => "\n    ")
-#   replace_h1_with_h3(text) = replace(text, r"(^|\n)# " => x -> "$(x.match)### ")
-#   space_indent2(str) = join("  " .* split(str, '\n'), "\n")
-
-#   cfg = report_config[]; (cfg === nothing) && error("No report config")
-
-#   if cfg.first_call
-#     cfg.first_call = false
-#     isfile(cfg.report_path) && rm(cfg.report_path)
-#   end
-
-#   msg = msg |> dedent |> turn_space_indent2_into4 |> replace_h1_with_h3 |> rstrip
-
-#   print_ && println(space_indent2(msg), "\n")
-
-#   mkpath(dirname(cfg.report_path))
-#   open(cfg.report_path, "a") do io
-#     write(io, msg * "\n\n")
-#   end
-#   nothing
-# end
