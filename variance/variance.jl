c5742ded30a8a2a3b950deb70ec057c3949e544b
alex <alex@pc.local>
2025-09-04 18:40:13 +1000

    upd

--- variance/variance.jl
+++ variance/variance.jl
@@ -26,15 +26,16 @@ Can variance of daily prices be measured with good precision?
 
 Daily prices have tail exponent ~3, so so `Var[Var]` (4th moment) is infinity. So Var has
 very slow convergence and can't be measured with good precision. Especially  on small samples,
-so current, point in time volatility estimators are questionable.
+so measures of current (point in time) volatility estimators are questionable.
 
 And so GARCH, EMA and similar approaches relying on Var or STD are also questionable.
 
-Yet, I think this is not true. Because stock prices have **conditional** variance, not i.i.d.
-And because of clusters of volatility, the variance measurement are more reliable.
+Yet, **I think this is not true**. Because stock prices have **conditional** variance (clusters of
+volatility), not i.i.d. And so, the variance measurements are more reliable.
 
-In this experiment i.i.d. variance measured, and indeed it has slow convergence, but, as I mentioned
-it may not apply to real stock prices.
+# Experiment
+
+Measuring convergence of Variance and MeanAbsDev of i.i.d. `StudentT(ν=3)`, indeed it has slow convergence. But, it may not apply to real stock prices having conditional variance.
 """)
 
 # Distribution -------------------------------------------------------------------------------------
